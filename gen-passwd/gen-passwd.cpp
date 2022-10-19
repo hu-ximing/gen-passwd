7fd08922dce464cfc6aa94888cc51c9adbe6ef72
hu-ximing <102689879+hu-ximing@users.noreply.github.com>
2022-10-19 09:58:31 -0500

    Add project files.

---
+++ gen-passwd/gen-passwd.cpp
@@ -0,0 +1,44 @@
+// gen-passwd.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <algorithm>
+#include <cctype>
+#include <iostream>
+#include <random>
+#include <string>
+
+using namespace std;
+
+std::random_device rd;
+std::mt19937_64 mt(rd());
+std::uniform_int_distribution<int> dist(33, 126);
+
+bool validate(string p) {
+	return any_of(p.begin(), p.end(), [](int i) { return isupper(i); })
+		&& any_of(p.begin(), p.end(), [](int i) { return islower(i); })
+		&& any_of(p.begin(), p.end(), [](int i) { return isdigit(i); })
+		&& count_if(p.begin(), p.end(), [](int i) { return ispunct(i); }) >= 1
+		&& count_if(p.begin(), p.end(), [](int i) { return ispunct(i); }) < p.length() / 5;
+}
+
+string gen_password(int len) {
+	string p(len, 0);
+	int max_attempt = 1024;
+	for (int ct = 0; ct < max_attempt; ct++) {
+		generate(p.begin(), p.end(), []() { return dist(mt); });
+		if (validate(p))
+			return p;
+	}
+	return "Failed to generate password. max_attempt: " + to_string(max_attempt);
+}
+
+int main(int argc, char** argv)
+{
+	int len;
+	if (argc == 1)
+		len = 18;
+	else
+		len = atoi(argv[1]);
+
+	cout << gen_password(len) << '\n';
+}
